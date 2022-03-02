f5468e5ff49e493651287f4020a283fcdb1a46d4
Carrette Camille <camille.carrette@etu.umontpellier.fr>
2022-03-02 15:55:08 +0100

    grille.cpp fonctionnel

--- Grille.cpp
+++ Grille.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+void RandPermut(int *ent, int n) { 
+        for (int r, i=n; i>0;) {
+            int e=ent[r=rand()%i]; 
+            ent[r]=ent[--i]; 
+            ent[i]=e;}
+    }
+
 int main(){
     int grille[9][9];
     int tmp = 1;
@@ -20,11 +27,13 @@ int main(){
     //première ligne
     srand((unsigned int)time(0));
     int val;
-    for(int i = 1 ; i < 9 ; i++){
-        val =rand() % 9 +1;
-        grille[0][val]=i;
+    for(int i = 0 ; i < 9 ; i++){
+        grille[0][i]=i+1;
     }
+    
+    RandPermut(grille[0],9); //permutation aléatoire des entiers de la première ligne
 
+    //remplissage des premières lignes
     for(int i = 1; i < 9; i++){
         for(int j = 0; j < 9; j++){
             if(i<3){
@@ -33,12 +42,22 @@ int main(){
                 }else{
                     grille[i][j]=grille[i-1][j+6];
                 }
-                }else{
-                //grille[i][j]=0;
             }
         }
     }
 
+    //remplplissage des colones
+    for(int i = 1; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(i >= 3){
+                    if(j % 3 == 0){
+                        grille[i][j]=grille[i-3][j+2];
+                    }else{
+                        grille[i][j]=grille[i-3][j-1];
+                    }
+                }
+            }
+        }
 
 
 
